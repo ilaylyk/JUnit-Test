4cac4acd9e9fd190a58b1a82e0a9bc137562c6c3
ilaylyk <40176935+ilaylyk@users.noreply.github.com>
2021-03-30 14:15:16 +0300

    Revert "JUnit"

--- src/main/java/Program.java
+++
@@ -1,7 +0,0 @@
-import java.util.Arrays;
-
-public class Program {
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(QuadraticEquationCoefficients.quadraticQequation(1, 6, 5)));
-    }
-}
